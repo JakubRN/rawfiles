4f5c1a402636e212dce6938c7d4c06b240bd0cfc
JakubRN <jakub1niedzwiedzki@gmail.com>
2021-05-14 13:07:21 +0200

    Add simple camera picture store

---
+++ camera_picture_store.py
@@ -0,0 +1,91 @@
+# --------------------------------------------------------
+# Camera sample code for Tegra X2/X1
+#
+# This program could capture and display video from
+# IP CAM, USB webcam, or the Tegra onboard camera.
+# Refer to the following blog post for how to set up
+# and run the code:
+#   https://jkjung-avt.github.io/tx2-camera-with-python/
+#
+# Written by JK Jung <jkjung13@gmail.com>
+# --------------------------------------------------------
+
+
+import sys
+import argparse
+import cv2
+import time
+
+WINDOW_NAME = 'CameraDemo'
+
+
+def open_window(width, height):
+    cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(WINDOW_NAME, width, height)
+    cv2.moveWindow(WINDOW_NAME, 0, 0)
+    cv2.setWindowTitle(WINDOW_NAME, 'Albatros camera view')
+
+
+def read_cam(cap):
+    show_help = True
+    full_scrn = False
+    help_text = '"Esc" to Quit, "H" for Help, "F" to Toggle Fullscreen'
+    font = cv2.FONT_HERSHEY_PLAIN
+    
+    print('expected FPS: ', cap.get(cv2.CAP_PROP_FPS))
+                       
+    currtime = time.time()
+    counter = 0
+    while True:
+        counter += 1
+        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
+            # Check to see if the user has closed the window
+            # If yes, terminate the program
+            break
+        if(time.time() - currtime > 1):
+            print("FPS: ", counter)
+            counter = 0
+            currtime = time.time()
+
+        _, img = cap.read() # grab the next image frame from camera
+        if show_help:
+            cv2.putText(img, help_text, (11, 20), font,
+                        1.0, (32, 32, 32), 4, cv2.LINE_AA)
+            cv2.putText(img, help_text, (10, 20), font,
+                        1.0, (240, 240, 240), 1, cv2.LINE_AA)
+        cv2.imshow(WINDOW_NAME, img)
+        key = cv2.waitKey(1)
+        if key == 27: # ESC key: quit program
+            break
+        elif key == ord('H') or key == ord('h'): # toggle help message
+            show_help = not show_help
+        elif key == ord('F') or key == ord('f'): # toggle fullscreen
+            full_scrn = not full_scrn
+            if full_scrn:
+                cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
+                                      cv2.WINDOW_FULLSCREEN)
+            else:
+                cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
+                                      cv2.WINDOW_NORMAL)
+
+        
+def readCamera(dev):
+    cap = cv2.VideoCapture(dev)
+    cap.set(3,960)     #horizontal pixels
+    cap.set(4,540)     #vertical pixels
+    # cap.set(5, 5)      #FPS
+    if not cap.isOpened():
+        sys.exit('Failed to open camera!')
+    cap.set(cv2.CAP_PROP_BUFFERSIZE,5)
+    w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    print(w, ", ", h)
+    open_window(int(w), int(h))
+    read_cam(cap)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    readCamera("/dev/video0")
