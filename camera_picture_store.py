15553e6b1460e0417e7ca3d58673c99881dc8216
JakubRN <jakub1niedzwiedzki@gmail.com>
2021-05-14 14:44:17 +0200

    Add Gstreamer

--- camera_picture_store.py
+++ camera_picture_store.py
@@ -17,7 +17,8 @@ import cv2
 import time
 
 WINDOW_NAME = 'CameraDemo'
-
+img_width = 0
+img_height = 0
 
 def open_window(width, height):
     cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
@@ -47,12 +48,13 @@ def read_cam(cap):
             counter = 0
             currtime = time.time()
 
-        ret, img = cap.read() # grab the next image frame from camera
+        ret, img_org = cap.read() # grab the next image frame from camera
         
         # if frame is read correctly ret is True
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
+        img = cv2.resize(img_org, ((img_width/4), int(img_height/4)))
         if show_help:
             cv2.putText(img, help_text, (11, 20), font,
                         1.0, (32, 32, 32), 4, cv2.LINE_AA)
@@ -74,19 +76,23 @@ def read_cam(cap):
                                       cv2.WINDOW_NORMAL)
 
         
+def open_cam_usb(dev, width, height):
+    # We want to set width and height here, otherwise we could just do:
+    #     return cv2.VideoCapture(dev)
+    gst_str = ("v4l2src device=/dev/video{} ! "
+               "video/x-raw, width=(int){}, height=(int){}, format=(string)RGB ! "
+               "videoconvert ! appsink").format(dev, width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
 def readCamera(dev):
-    cap = cv2.VideoCapture(dev)
-    cap.set(3, 1920)     #horizontal pixels
-    cap.set(4, 1080)     #vertical pixels
-    cap.set(5, 30)      #FPS
+    cap = open_cam_usb(dev,1920, 1080)
     if not cap.isOpened():
         sys.exit('Failed to open camera!')
-    cap.set(cv2.CAP_PROP_BUFFERSIZE,5)
-    w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    img_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    img_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     print("start reading at FPS ", cv2.CAP_PROP_FPS)
-    print(w, ", ", h)
-    open_window(int(w), int(h))
+    print(img_width, ", ", img_height)
+    open_window(int(img_width/4), int(img_height/4))
     read_cam(cap)
 
     cap.release()
@@ -94,4 +100,4 @@ def readCamera(dev):
 
 
 if __name__ == '__main__':
-    readCamera("/dev/video0")
+    readCamera("0")
